c8927a6bf463ba52bf06155c2a8e9084452fd032
Christopher Browne <cbbrowne@ca.afilias.info>
2010-08-26 12:52:06 -0400

    Drew in material from Ivan's (not-evidently-SCMed) head, particularly including Makefile and such

---
+++ elisp.mk
@@ -0,0 +1,23 @@
+INSTALL = /usr/bin/install -c
+
+EMACS= emacs --no-init-file --no-site-file -batch \
+--eval "(setq load-path (cons \".\" load-path))" $(EMACSARG)
+
+AUTOLOAD= --eval "(setq load-path (cons \"..\" load-path))" \
+--eval "(require 'ivan-autoload)" \
+--eval "(let ((generated-autoload-file (expand-file-name \"$@\"))) \
+  (mapcar (function update-file-autoloads) command-line-args-left) \
+  (save-buffers-kill-emacs t))"
+
+%.elc: %.el
+	@$(EMACS) -f batch-byte-compile $<
+
+
+install: $(ELC) $(AUTOFILE).elc
+	@mkdir -p -m 0755 $(INSTALLDIR) ;                    \
+    for i in $(ELC) $(AUTOFILE) ; do                             \
+         $(INSTALL) $$i $(INSTALLDIR) ;                  \
+    done ;                                               \
+
+clean:
+	rm -f $(ELC) $(AUTOFILE).el $(AUTOFILE).elc
